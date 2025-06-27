be31546b2476dcbf4913e674afb96eff806cd157
Carter Hinsley <carterhinsley@outlook.com>
2025-06-27 00:06:37 -0400

    Fix the double tent map

--- maps/double_tent.jl
+++ maps/double_tent.jl
@@ -1,6 +1,6 @@
 # A double tent map on [0, 1].
-# The map is piecewise linear on [0, 0.25], [0.25, 0.5], [0.5, 0.75], and [0.75, 1].
-# f(0) = 0, f(1) = 1, f(0.5) = 0.5.
+# The map is piecewise linear on [0, 0.25], [0.25, 0.75], and [0.75, 1].
+# f(0) = 0, f(1) = 1.
 # The parameters p = [a, b] are the heights at x = 0.25 and x = 0.75, respectively.
 # So, f(0.25) = a and f(0.75) = b.
 
@@ -10,12 +10,11 @@ function double_tent_map(p, x, n_iter=1)
     for _ in 1:n_iter
         if 0 <= x < 0.25
             x = 4 * a * x
-        elseif 0.25 <= x < 0.5
-            x = (2 - 4 * a) * x + 2 * a - 0.5
-        elseif 0.5 <= x < 0.75
-            x = (4 * b - 2) * x - 2 * b + 1.5
+        elseif 0.25 <= x < 0.75
+            slope = (b - a) / 0.5
+            x = slope * (x - 0.25) + a
         elseif 0.75 <= x <= 1
-            x = (4 - 4 * b) * x + 4 * b - 3
+            x = (4 - 4 * b) * (x - 0.75) + b
         end
     end
     return x
@@ -23,5 +22,5 @@ end
 
 # Define the critical points of the map.
 function critical_points(p)
-  return [0.25, 0.5, 0.75]
+  return [0.25, 0.75]
 end
