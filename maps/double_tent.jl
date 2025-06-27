4bc996ec58cd14296f407ba51fffe535e2236b88
Carter Hinsley <carterhinsley@outlook.com>
2025-06-27 00:29:44 -0400

    Use 1/3 and 2/3 as critical points of double tent map

--- maps/double_tent.jl
+++ maps/double_tent.jl
@@ -1,20 +1,21 @@
 # A double tent map on [0, 1].
-# The map is piecewise linear on [0, 0.25], [0.25, 0.75], and [0.75, 1].
+# The map is piecewise linear on [0, 1/3], [1/3, 2/3], and [2/3, 1].
 # f(0) = 0, f(1) = 1.
-# The parameters p = [a, b] are the heights at x = 0.25 and x = 0.75, respectively.
-# So, f(0.25) = a and f(0.75) = b.
+# The parameters p = [a, b] are the heights at x = 1/3 and x = 2/3, respectively.
+# So, f(1/3) = a and f(2/3) = b.
 
 # Define the map.
 function double_tent_map(p, x, n_iter=1)
     a, b = p
     for _ in 1:n_iter
-        if 0 <= x < 0.25
-            x = 4 * a * x
-        elseif 0.25 <= x < 0.75
-            slope = (b - a) / 0.5
-            x = slope * (x - 0.25) + a
-        elseif 0.75 <= x <= 1
-            x = (4 - 4 * b) * (x - 0.75) + b
+        if 0 <= x < 1/3
+            x = 3 * a * x
+        elseif 1/3 <= x < 2/3
+            slope = 3 * (b - a)
+            x = slope * (x - 1/3) + a
+        elseif 2/3 <= x <= 1
+            slope = 3 * (1 - b)
+            x = slope * (x - 2/3) + b
         end
     end
     return x
@@ -22,5 +23,5 @@ end
 
 # Define the critical points of the map.
 function critical_points(p)
-  return [0.25, 0.75]
+  return [1/3, 2/3]
 end
